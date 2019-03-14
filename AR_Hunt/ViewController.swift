4b55a5563379ed10e242304076bad9994da19df0
sorianog <13261711+sorianog@users.noreply.github.com>
2019-03-13 20:15:13 -0400

    Added a camera preview

--- AR_Hunt/ViewController.swift
+++ AR_Hunt/ViewController.swift
@@ -22,16 +22,20 @@
 
 import UIKit
 import SceneKit
+import AVFoundation
 
 class ViewController: UIViewController {
   
   @IBOutlet weak var sceneView: SCNView!
   @IBOutlet weak var leftIndicator: UILabel!
   @IBOutlet weak var rightIndicator: UILabel!
+  var cameraSession: AVCaptureSession?
+  var cameraLayer: AVCaptureVideoPreviewLayer?
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    loadCamera()
+    self.cameraSession?.startRunning()
   }
   
   override func didReceiveMemoryWarning() {
@@ -39,6 +43,51 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
+  func createCaptureSession() -> (session: AVCaptureSession?, error: NSError?) {
+    var error: NSError?
+    var captureSession: AVCaptureSession?
+    let backVideoDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .back)
+    
+    if backVideoDevice != nil {
+      var videoInput: AVCaptureDeviceInput!
+      do {
+        videoInput = try AVCaptureDeviceInput(device: backVideoDevice)
+      } catch let err as NSError {
+        error = err
+        videoInput = nil
+      }
+      
+      if error == nil {
+        captureSession = AVCaptureSession()
+        if captureSession!.canAddInput(videoInput) {
+          captureSession!.addInput(videoInput)
+        } else {
+          error = NSError(domain: "", code: 0, userInfo: ["description": "Error adding video input."])
+        }
+      } else {
+        error = NSError(domain: "", code: 1, userInfo: ["description": "Error creating capture device input."])
+      }
+    } else {
+      error = NSError(domain: "", code: 2, userInfo: ["description": "Back video device not found."])
+    }
+    return (session: captureSession, error: error)
+  }
   
+  func loadCamera() {
+    let captureSessionResult = createCaptureSession()
+    guard captureSessionResult.error == nil, let session = captureSessionResult.session else {
+      print("Error creating capture session.")
+      return
+    }
+    self.cameraSession = session
+    
+    if let cameraLayer = AVCaptureVideoPreviewLayer(session: self.cameraSession) {
+      cameraLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+      cameraLayer.frame = self.view.bounds
+      
+      self.view.layer.insertSublayer(cameraLayer, at: 0)
+      self.cameraLayer = cameraLayer
+    }
+  }
 }
 
